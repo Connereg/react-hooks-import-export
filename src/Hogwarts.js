4e9972a232a0c57bae67cfb3498618cb62f41b74
Conner Gleason <connereg@optonline.net>
2021-08-29 16:22:49 -0400

    commit tree 10b0c048ea0d0f290b531f15e9e34cfd6099e5c8

--- src/Hogwarts.js
+++ src/Hogwarts.js
@@ -1,8 +1,20 @@
 import React from "react";
 import whoseHouse from "./houses/whoseHouse";
+import Hufflepuff from "./houses/Hufflepuff";
+import * as GryffFunctions from "./houses/Gryffindor";
+//import {colors, values, gryffMascot};
 
+console.log(GryffFunctions.colors)
+
+GryffFunctions.gryffMascot();
+
+GryffFunctions.values();
+
+
+/*
 function Hogwarts() {
   whoseHouse(); // => "HAGRID'S HOUSE!"
 
   return <h1>Welcome to Hogwarts!</h1>;
 }
+*/
